1b5ef15d647b2c304d074ed341820b49c64dc80a
william <williamwambugu663@gmail.com>
2025-05-25 07:35:19 +0300

    CRUD

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,19 +1,70 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.orm import declarative_base
+from datetime import datetime
+from sqlalchemy import (
+    create_engine, desc,
+    CheckConstraint, PrimaryKeyConstraint, UniqueConstraint,
+    Index, Column, DateTime, Integer, String
+)
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 Base = declarative_base()
 
 class Student(Base):
     __tablename__ = 'students'
+    __table_args__ = (
+        PrimaryKeyConstraint(
+            'id',
+            name='id_pk'),
+        UniqueConstraint(
+            'email',
+            name='unique_email'),
+        CheckConstraint(
+            'grade BETWEEN 1 AND 12',
+            name='grade_between_1_and_12'))
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
+    Index('index_name', 'name')
+
+    id = Column(Integer())
+    name = Column(String())
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
+
+    def __repr__(self):
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
 
 if __name__ == '__main__':
-    # Create an SQLite database in memory.
-    engine = create_engine('sqlite:///students.db', echo=True)
 
-    # Create the tables in the database (based on the Base metadata)
+    engine = create_engine('sqlite:///:memory:')
     Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    albert_einstein = Student(
+    name="Albert Einstein",
+    email="albert.einstein@zurich.edu",
+    grade=6,
+    birthday=datetime(
+        year=1879,
+        month=3,
+        day=14
+    )
+)
+
+alan_turing = Student(
+    name="Alan Turing",
+    email="alan.turing@sherborne.edu",
+    grade=11,
+    birthday=datetime(
+        year=1912,
+        month=6,
+        day=23
+    )
+)
+session.bulk_save_objects([albert_einstein, alan_turing])
+session.commit()
